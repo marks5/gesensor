663bf259123ff66e62638ed9d980837bb1be0184
Gabriel Marques <marques18@gmail.com>
2018-11-28 03:26:42 -0300

    Updates

---
+++ precache-manifest.c11ef8c72d0f5893cbee01ea7d8bde7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a7c03e80a3f3d069a3c",
+    "url": "/gesensor/static/js/runtime~main.8a7c03e8.js"
+  },
+  {
+    "revision": "61662194ceba947c3da1",
+    "url": "/gesensor/static/js/main.61662194.chunk.js"
+  },
+  {
+    "revision": "3f0eeff5bbae15004611",
+    "url": "/gesensor/static/js/1.3f0eeff5.chunk.js"
+  },
+  {
+    "revision": "61662194ceba947c3da1",
+    "url": "/gesensor/static/css/main.3adddd75.chunk.css"
+  },
+  {
+    "revision": "d2ed1cf633a183111feecab12b27995d",
+    "url": "/gesensor/index.html"
+  }
+];
